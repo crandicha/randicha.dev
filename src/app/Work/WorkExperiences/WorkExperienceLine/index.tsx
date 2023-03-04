88ea27e49a8feac8c0fe6b0469858b40495ab14b
Charles Randicha <charles.randicha@gmail.com>
2023-03-04 16:08:15 +0700

    fix scrolly

--- src/app/Work/WorkExperiences/WorkExperienceLine/index.tsx
+++ src/app/Work/WorkExperiences/WorkExperienceLine/index.tsx
@@ -8,9 +8,6 @@ const WorkExperienceLine = () => {
     target: ref,
     offset: ['start 80%', '40% 40%'],
   })
-  const scaleY = React.useMemo(() => {
-    return scrollYProgress
-  }, [scrollYProgress])
 
   return (
     <motion.div
@@ -18,7 +15,7 @@ const WorkExperienceLine = () => {
       className="absolute top-[14%] left-[50%] z-[100] h-[72%] w-[1px] border-l border-r border-tertiary"
       style={{
         transformOrigin: 'top',
-        scaleY,
+        scaleY: scrollYProgress,
       }}
     ></motion.div>
   )
